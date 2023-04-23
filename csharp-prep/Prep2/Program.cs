bbf301064095e7f3bae7473b701f93378d161f01
gavin-cannon <gavincannon@byui.edu>
2023-04-22 18:14:11 -0600

    Did the assignments.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        int percentage = int.Parse(Console.ReadLine());
+        string letter = " ";
+        if (percentage >= 90){
+            letter = "A";
+        }
+        else if (percentage >= 80 && percentage < 90){
+            letter = "B";
+        }
+        else if (percentage >= 70 && percentage < 80){
+            letter = "C";
+        }
+        else if (percentage >= 60){
+            letter = "D";
+        }
+        else{
+            letter = "F";
+        }
+        Console.WriteLine($"You got a {letter}.");
+        if (percentage >= 70){
+            Console.WriteLine("You passed!");
+        }
+        else{
+            Console.WriteLine("You failed.");
+        }
     }
 }
