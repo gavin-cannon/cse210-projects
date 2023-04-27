f8f1a6016aa1c76d654a440e28058f05facd93a1
gavin-cannon <gavincannon@byui.edu>
2023-04-26 23:52:23 -0600

    Finished the prep 4 assignment.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
+        List<int> numbers = new List<int>();
         Console.WriteLine("Hello Prep4 World!");
+        int stopNumber = 1;
+        while (stopNumber != 0){
+            
+            Console.Write("Please enter a number.");
+            stopNumber = int.Parse(Console.ReadLine());
+            if (stopNumber != 0){
+            numbers.Add(stopNumber);
+            
+            };
+        };
+    
+            int total = 0;
+            float average = 0;
+            int maxNumber = 0;
+            foreach (int number in numbers){
+            
+            
+            total += number;
+            average = ((float)total) / numbers.Count;
+            if(maxNumber < number){
+                maxNumber = number;
+            };
+            
+        };
+        Console.WriteLine($"Total: {total}");
+        Console.WriteLine($"Average: {average}");
+        Console.WriteLine($"Max Number: {maxNumber}");
+        
     }
 }
