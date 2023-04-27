732af41f785bb755afdf69656b28ff0b93935ca0
gavin-cannon <gavincannon@byui.edu>
2023-04-26 23:24:49 -0600

    Did the Prep 3 assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,23 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        Random rnd = new Random();
+        int magicNumber = rnd.Next(1, 100);
+        // Console.Write("Magic Number? ");
+        // int magicNumber = int.Parse(Console.ReadLine());
+        Console.Write("What is your guess? ");
+        int guessNumber = int.Parse(Console.ReadLine());
+        bool loop = false;
+        while (loop == false){
+            if (guessNumber == magicNumber){
+                loop = true;
+                Console.WriteLine("Congrats.");
+            }
+            else{
+                Console.WriteLine("Try again.");
+                Console.Write("What is your guess? ");
+                guessNumber = int.Parse(Console.ReadLine());
+            }
+        };
     }
 }
