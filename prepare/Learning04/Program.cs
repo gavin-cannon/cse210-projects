433bce2d19af28fdd4a3e6e474366bb4e98152ec
gavin-cannon <gavincannon@byui.edu>
2023-05-29 22:07:29 -0600

    I wrote all the classes and all that. Did a good job too.

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,5 +5,32 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Samuel Bennett", "Multiplication");
+        
+        
+        string summary = assignment1.GetSummary();
+        Console.WriteLine(summary);
+        Console.WriteLine();
+
+
+        MathAssignment math1 = new MathAssignment("Robert Rodriguez", "Fractions", "Section 7.3", "Problems 8-19");
+        string mathHomework = math1.GetHomeworkList();
+        string mathSum = math1.GetSummary();
+        Console.WriteLine(mathSum);
+        Console.WriteLine(mathHomework);
+
+
+        WritingAssignment writeAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II by Mary Waters");
+        string titleGet = writeAssignment.GetWritingInformation();
+        string writeSummary = writeAssignment.GetSummary();
+        Console.WriteLine();
+        Console.WriteLine(writeSummary);
+        Console.WriteLine(titleGet);
+        
+
+
+
+
+
     }
 }
